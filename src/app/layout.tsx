60d081e3fcabe282f48048525c698367c2168aa0
mjarkk <mkopenga@gmail.com>
2025-03-23 18:08:09 +0100

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,15 @@ const geist = Geist({
 
 export const metadata: Metadata = {
   title: "Ontshitten.nl | Ont-shit uw leven!",
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: "/favicon-32x32.png",
+  },
   description: "De ultieme gids voor het ontshitten van uw dagelijks leven",
   openGraph: {
     title: "🚨 Ontshitten.nl | Bevrijdt Uzelf Van De Shit-Apocalyps! 🚨",
-    description: "Verdrinkt u in een tsunami van existentiële uitwerpselen? Ontdek onze REVOLUTIONAIRE ontshit-methodologie die uw bestaan zal transformeren... Durf u door te klikken?",
-    images: [
-      {
-        url: "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        width: 1200,
-        height: 630,
-        alt: "Ontshitten.nl - Uw pad naar verlichting"
-      }
-    ],
+    description:
+      "Verdrinkt u in een tsunami van existentiële uitwerpselen? Ontdek onze REVOLUTIONAIRE ontshit-methodologie die uw bestaan zal transformeren... Durf u door te klikken?",
     type: "website",
     locale: "nl_NL",
   },
@@ -32,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
-      <body className={`${geist.className} antialiased`}>
-        {children}
-      </body>
+      <body className={`${geist.className} antialiased`}>{children}</body>
     </html>
   );
 }
